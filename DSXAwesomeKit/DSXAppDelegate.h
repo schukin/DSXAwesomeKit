d16a1e43450c8bf320ec69543a02b35307b93b2d
Dave Schukin <dave@clinkle.com>
2014-01-05 23:34:00 -0800

    Initial Commit

---
+++ DSXAwesomeKit/DSXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DSXAppDelegate.h
+//  DSXAwesomeKit
+//
+//  Created by David Schukin on 1/5/14.
+//  Copyright (c) 2014 Dave Schukin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DSXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DSXAwesomeKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DSXAwesomeKit
+//
+//  Created by David Schukin on 1/5/14.
+//  Copyright (c) 2014 Dave Schukin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DSXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DSXAppDelegate class]));
+    }
+}
